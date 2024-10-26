3dd5f01d407c44faf6c584c83779bfb92963e038
HDAdminModule <fatimashoma44@gmail.com>
2024-10-26 09:20:14 -0400

    Update ExecHub.lua

--- ExecHub.lua
+++ ExecHub.lua
@@ -1,58 +1,7 @@
---toggle button yay--
-local gui = Instance.New("ScreenGUI",game.PlayerGUI)
-local frame = Instance.New("Frame",gui)
-frame.BackgroundTransparency = 1
-local TButton = Instance.New("TextButton",frame)
-local TButtonDrag = Instance.New("LocalScript",TButton)
-LocalScript.Name = STButtonDrag
-STButtonDrag.source = ({
-local function MakeDraggable(DragPoint, Main)
-    pcall(function()
-        local Dragging, DragInput, MousePos, FramePos = false
 
-        AddConnection(DragPoint.InputBegan, function(Input)
-            if Input.UserInputType == Enum.UserInputType.MouseButton1 or Input.UserInputType == Enum.UserInputType.Touch then
-                Dragging = true
-                MousePos = Input.Position
-                FramePos = Main.Position
+local OrionLib = loadstring(game:HttpGet(('https://pastebin.com/raw/WRUyYTdY')))()
 
-                Input.Changed:Connect(function()
-                    if Input.UserInputState == Enum.UserInputState.End then
-                        Dragging = false
-                    end
-                end)
-            end
-        end)
-
-        AddConnection(DragPoint.InputChanged, function(Input)
-            if Input.UserInputType == Enum.UserInputType.MouseMovement or Input.UserInputType == Enum.UserInputType.Touch then
-                DragInput = Input
-            end
-        end)
-
-        AddConnection(UserInputService.InputChanged, function(Input)
-            if Input == DragInput and Dragging then
-                local Delta = Input.Position - MousePos
-                Main.Position = UDim2.new(
-                    FramePos.X.Scale, FramePos.X.Offset + Delta.X,
-                    FramePos.Y.Scale, FramePos.Y.Offset + Delta.Y
-                )
-            end
-        end)
-    end)
-end 
-)}
-MakeDraggable
-TButton.Text = Toggle GUI
-TButton.Size.Y = 0.5
-TButton.Size.X = 0.5
-local TCorners = Instance.New("UICorners",TButton)
-local STButton = Instance.New("LocalScript",TButton)
-STButton.source = local UserInputService = game:GetService('UserInputService')
---Orion Library down here!!--
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-
-local Window = OrionLib:MakeWindow({Name = "Executors", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "Executors", HidePremium = false, SaveConfig = true, ConfigFolder = nil})
 
 local Tab1 = Window:MakeTab({
 	Name = "Executors",
@@ -106,6 +55,14 @@ Tab1:AddButton({
       		loadstring(game:HttpGet("https://raw.githubusercontent.com/Giangplay/Script/main/Arceus_X_V3.lua"))()
   	end    
 })
+local Section = Tab1:AddSection({
+    Name = "Keyboard"
+})
+Tab:AddButton({
+    Name = "[Built-In Keyboard] Rochips Universal",
+    Callback = function()
+    end
+})
 
 OrionLib:MakeNotification({
 	Name = "i stole",
@@ -113,4 +70,5 @@ OrionLib:MakeNotification({
 	Image = "rbxassetid://4483345998",
 	Time = 5
 })
+
 
